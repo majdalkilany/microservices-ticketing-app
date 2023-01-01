635c28a5483cc1c17f86b6d4f91c03e766dcd92e
majdalkilany <majdkilany92@gmail.com>
2023-01-01 16:09:05 +0300

    create common npm  packge

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express, { json } from 'express';
+import cookieSession from 'cookie-session';
+import 'express-async-errors';
+
+import { currentUserRouter } from './routes/current-user.routs';
+import { signUpRouter } from './routes/sign-up';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { errorHandler } from '@majd-tickets/common';
+import { NotFoundError } from '@majd-tickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+// routs
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+// middleware
+
+app.use(errorHandler);
+
+export { app };
